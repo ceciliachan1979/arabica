79419977f7ac7e187d2b4d43f1e864c95a256bf8
gapry <gapry@protonmail.com>
2024-06-03 03:43:14 +0800

    test_suite: code refactoring

--- test/cpu/cpu_test_suite.hpp
+++ test/cpu/cpu_test_suite.hpp
@@ -4,8 +4,26 @@
 #include <arabica/memory/memory.hpp>
 #include <gtest/gtest.h>
 
-class cpu_test_suite : public ::testing::Test {
-public:
-  static void test_jump(void);
-  static void test_call(void);
-};
+#define arabica_cpu_test(test_case, content) \
+  TEST(cpu_test_suite, test_case) {          \
+    arabica::CPU cpu;                        \
+    arabica::Memory memory;                  \
+    content                                  \
+  }
+// clang-format off
+
+arabica_cpu_test(test_jump,  
+  memory.write(0x200, 0x16); 
+  memory.write(0x201, 0x00);
+  cpu.run(memory);
+  ASSERT_EQ(cpu.pc, 0x600);
+)
+
+arabica_cpu_test(test_call,
+  memory.write(0x200, 0x26);
+  memory.write(0x201, 0x00);
+  cpu.run(memory);
+  ASSERT_EQ(cpu.pc, 0x600);
+  ASSERT_EQ(cpu.stack.size(), 1);
+  ASSERT_EQ(cpu.stack.top(), 0x202);
+)
--- test/memory/memory_test_suite.hpp
+++ test/memory/memory_test_suite.hpp
@@ -4,12 +4,27 @@
 #include <arabica/memory/memory.hpp>
 #include <gtest/gtest.h>
 
-class memory_test_suite : public ::testing::Test {
-public:
-  static void write_and_assert(const arabica::Memory::address_t addr, const arabica::Memory::value_t val);
+void write_and_assert(const arabica::Memory::address_t address, const arabica::Memory::value_t value) {
+  arabica::Memory memory;
+  memory.write(address, value);
+  ASSERT_EQ(memory.read(address), value);
+}
 
-  static arabica::CPU cpu;
-  static arabica::Memory memory;
-  static arabica::Memory::address_t address;
-  static arabica::Memory::value_t value;
-};
+#define arabica_memory_test(test_case, content) \
+  TEST(memory_test_suite, test_case) {          \
+    arabica::CPU cpu;                           \
+    content                                     \
+  }
+// clang-format off
+
+arabica_memory_test(pc_test_case_001,
+  write_and_assert(cpu.pc, 0x61);
+)
+
+arabica_memory_test(pc_test_case_002,
+  write_and_assert(cpu.pc, 0x00);
+)
+
+arabica_memory_test(address_write_test_case_001,
+  write_and_assert(0x300, 0x11);
+)

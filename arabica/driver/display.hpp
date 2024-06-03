d0f14b76614088a6b51cf7db7b51f5888f2e6161
gapry <gapry@protonmail.com>
2024-06-03 20:20:16 +0800

    display: add noncopyable and move semantics

--- arabica/driver/display.hpp
+++ arabica/driver/display.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <arabica/type/noncopyable.hpp>
+
+namespace arabica {
+
+class display : public noncopyable {
+public:
+  display(display&&)            = default;
+  display& operator=(display&&) = default;
+};
+
+} // namespace arabica

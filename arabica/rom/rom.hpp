64fedc57dc910237c80399c5db59a5209314c529
gapry <gapry@protonmail.com>
2024-06-03 20:19:04 +0800

    rom: add noncopyable and move semantics

--- arabica/rom/rom.hpp
+++ arabica/rom/rom.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <arabica/type/noncopyable.hpp>
+
+namespace arabica {
+
+class rom : public noncopyable {
+public:
+  rom(rom&&)            = default;
+  rom& operator=(rom&&) = default;
+};
+
+} // namespace arabica

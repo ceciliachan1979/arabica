209e902d197ab3a772f44a23d4af17fe6c72e2d4
gapry <gapry@protonmail.com>
2024-06-03 18:52:50 +0800

    memory: add memmap document

--- arabica/memory/memory.hpp
+++ arabica/memory/memory.hpp
@@ -7,6 +7,31 @@
 
 namespace arabica {
 
+// spec: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#memmap
+//
+// +---------------+= 0xFFF (4095) End of Chip-8 RAM
+// |               |
+// |               |
+// |               |
+// |               |
+// |               |
+// | 0x200 to 0xFFF|
+// |     Chip-8    |
+// | Program / Data|
+// |     Space     |
+// |               |
+// |               |
+// |               |
+// |               |
+// |               |
+// |               |
+// |               |
+// +---------------+= 0x200 (512) Start of most Chip-8 programs
+// | 0x000 to 0x1FF|
+// | Reserved for  |
+// |  interpreter  |
+// +---------------+= 0x000 (0) Start of Chip-8 RAM
+
 class Memory : public noncopyable {
 public:
   using address_t = uint16_t;

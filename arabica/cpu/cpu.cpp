0ad423a1fdc34ad369be1b5bbb38b484f31819dc
gapry <gapry@protonmail.com>
2024-06-04 05:31:42 +0800

    cpu: decode the RET opcode and add corresponding test case

--- arabica/cpu/cpu.cpp
+++ arabica/cpu/cpu.cpp
@@ -4,8 +4,20 @@
 namespace arabica {
 
 void CPU::run(const Memory& memory) {
-  instruction = memory[pc] << 8 | memory[pc + 1];
-  opcode      = static_cast<OP_CODE>(instruction & 0xF000);
+  instruction     = memory[pc] << 8 | memory[pc + 1];
+  uint16_t prefix = instruction & 0xF000;
+  uint16_t suffix = instruction & 0x00FF;
+  opcode          = static_cast<OP_CODE>(prefix);
+
+  switch (prefix) {
+    case 0x0: {
+      switch (suffix) {
+        case 0xEE: opcode = OP_CODE::RET; break;
+        default:; break;
+      }
+    } break;
+    default: break;
+  }
 
   switch (opcode) {
     case OP_CODE::JP_addr: {
@@ -16,6 +28,14 @@ void CPU::run(const Memory& memory) {
       stack.push(pc + 2);
       uint16_t target = instruction & 0x0FFF;
       pc              = target;
+    } break;
+    case OP_CODE::RET: {
+      if (!stack.empty()) {
+        pc = stack.top();
+        stack.pop();
+      } else {
+        // ToDo: raise interrupt? how to test the failed case?
+      };
     } break;
     default: {
       fmt::print("Unknown opcode: 0x{:X}\n", static_cast<uint16_t>(opcode));

5becbd9f554f6bcee3a78b47f8d0a2d229577ac5
Kelvin Wu <kelvinyu1117@gmail.com>
2024-06-05 02:11:49 +0800

    fix: advancing the pc after instruction execution

--- arabica/cpu/cpu.cpp
+++ arabica/cpu/cpu.cpp
@@ -1,13 +1,17 @@
-#include "op_code.hpp"
 #include <arabica/cpu/cpu.hpp>
 #include <fmt/core.h>
 
 namespace arabica {
 
+inline static constexpr void advance_pc(uint16_t& pc, uint16_t offset = 2) {
+  pc += offset;
+}
+
 void CPU::run(const Memory& memory) {
   instruction     = memory[pc] << 8 | memory[pc + 1];
-  uint16_t prefix = (instruction & 0xF000) >> 12;
+  uint16_t prefix = instruction & 0xF000;
   uint16_t suffix = instruction & 0x00FF;
+  opcode          = static_cast<OP_CODE>(prefix);
 
   switch (prefix) {
     case 0x0: {
@@ -16,19 +20,7 @@ void CPU::run(const Memory& memory) {
         default:; break;
       }
     } break;
-    case 0x1: {
-      opcode = OP_CODE::JP_addr;
-    } break;
-    case 0x2: {
-      opcode = OP_CODE::CALL_addr;
-    } break;
-    case 0x6: {
-      opcode = OP_CODE::LD_Vx_byte;
-    } break;
-    case 0x7: {
-      opcode = OP_CODE::ADD_Vx_byte;
-    } break;
-    case 0x8: {
+    case 0x8000: {
       switch (instruction & 0x000F) {
         case 0x0: opcode = OP_CODE::LD_Vx_Vy; break;
         case 0x4: opcode = OP_CODE::ADD_Vx_Vy; break;
@@ -58,22 +50,30 @@ void CPU::run(const Memory& memory) {
     case OP_CODE::LD_Vx_byte: {
       uint8_t byte_v                         = instruction & 0x00FF;
       registers[(instruction & 0x0F00) >> 8] = byte_v;
+
+      advance_pc(pc);
     } break;
     case OP_CODE::ADD_Vx_byte: {
       uint8_t byte_v = instruction & 0x00FF;
       uint8_t x      = (instruction & 0x0F00) >> 8;
       registers[x]   = registers[x] + byte_v; // it will be wrapped if overflow occurs
+
+      advance_pc(pc);
     } break;
     case OP_CODE::LD_Vx_Vy: {
       uint8_t x    = (instruction & 0x0F00) >> 8;
       uint8_t y    = (instruction & 0x00F0) >> 4;
       registers[x] = registers[y];
+
+      advance_pc(pc);
     } break;
     case OP_CODE::ADD_Vx_Vy: {
       uint8_t x      = (instruction & 0x0F00) >> 8;
       uint8_t y      = (instruction & 0x00F0) >> 4;
       registers[x]   = registers[x] + registers[y];
       registers[0xF] = registers[y] > 255 - registers[x];
+
+      advance_pc(pc);
     } break;
     default: {
       fmt::print("Unknown opcode: 0x{:X}\n", static_cast<uint16_t>(opcode));
